df2c05bbcb851dccc95e8e55147bce09b15317ba
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2025-11-26 13:04:34 +0100

    Adding the spactateur class

---
+++ src/Spactateur.java
@@ -0,0 +1,36 @@
+class Spectateur {
+
+    private int spectateurId;
+    private String nom;
+    private String email;
+
+    public Spectateur(int spectateurId, String email, String nom) {
+        this.spectateurId = spectateurId;
+        this.email = email;
+        this.nom = nom;
+    }
+
+    public int getSpectateurId() {
+        return spectateurId;
+    }
+
+    public void setSpectateurId(int spectateurId) {
+        this.spectateurId = spectateurId;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
