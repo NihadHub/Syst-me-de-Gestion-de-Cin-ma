b7ed3f0b1eec7a8ed3eee052cc894c84bdc6258d
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2025-11-26 15:17:32 +0100

    Creating Connextion class that's responsable for binding connexion and executing queries

---
+++ src/helper/Connexion.java
@@ -0,0 +1,37 @@
+package helper;
+
+import java.sql.*;
+
+public class Connexion {
+    static final String USER = "root";
+    static final String PASS = "";
+    static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/cinemaDB";
+    Statement statement;
+
+    public Statement initConnexion() {
+        Connection connection = null;
+        try {
+
+            connection = DriverManager.getConnection(
+                    DB_URL,
+                    USER,
+                    PASS
+            );
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return statement;
+    }
+
+    public ResultSet executerUneRequete(String requete) {
+        ResultSet resultSet = null;
+        try {
+            resultSet = statement.executeQuery(requete);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+
+    }
+}
