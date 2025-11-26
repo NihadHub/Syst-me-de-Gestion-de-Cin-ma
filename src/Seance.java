4a7e6dfe3bd2d244a3ae4c88337ea252ed99eb2c
hiba <hibazekraoui23@email.com>
2025-11-26 17:23:07 +0100

    adding class seance

---
+++ src/Seance.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.sql.*;
+public class Seance {
+    private int seance_ID;
+    private String horaire;
+    private int salle;
+    private int capaciter;
+    //private ArrayList<Spectateurs> lesSpectateurs = new ArrayList<>();
+    Seance(int seance_ID,String horaire,int salle,int capaciter){
+        this.seance_ID = seance_ID;
+        this.horaire = horaire;
+        this.salle = salle;
+        this.capaciter = capaciter;
+    }
+
+    public int getSeance_ID() {
+        return seance_ID;
+    }
+
+    public void setSeance_ID(int seance_ID) {
+        this.seance_ID = seance_ID;
+    }
+
+    public String getHoraire() {
+        return horaire;
+    }
+
+    public void setHoraire(String horaire) {
+        this.horaire = horaire;
+    }
+
+    public int getSalle() {
+        return salle;
+    }
+
+    public void setSalle(int salle) {
+        this.salle = salle;
+    }
+
+    public int getCapaciter() {
+        return capaciter;
+    }
+
+    public void setCapaciter(int capaciter) {
+        this.capaciter = capaciter;
+    }
+}
