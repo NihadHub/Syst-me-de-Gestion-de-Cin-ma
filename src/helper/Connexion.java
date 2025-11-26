53670ab4d515f5eb104bc66b0629aa35b6b3a309
outeglaout jaouad <outeglaoutjaouad@gmail.com>
2025-11-26 17:10:14 +0100

    Adding main Class

--- src/helper/Connexion.java
+++ src/helper/Connexion.java
@@ -6,9 +6,9 @@ public class Connexion {
     static final String USER = "root";
     static final String PASS = "";
     static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/cinemaDB";
-    Statement statement;
+    public Statement statement;
 
-    public Statement initConnexion() {
+    public void initConnexion() {
         try {
 
             Connection connection = DriverManager.getConnection(
@@ -20,17 +20,15 @@ public class Connexion {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return statement;
     }
 
-    public ResultSet executerUneRequete(String requete) {
+    public void executerUneRequete(String requete) {
         ResultSet resultSet = null;
         try {
             resultSet = statement.executeQuery(requete);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return resultSet;
 
     }
 }
